438f9b5fd2ebe0c13bbf1fa6e16524133c5db5dc
Chinedu <chinedudaniel7@gmail.com>
2015-11-09 16:07:26 +0100

    Clean up user model and users controller and fix issues raised by Hound Ci

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,29 @@ class User < ActiveRecord::Base
   VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\.]+[\w+]\.[a-z]+\z/i
 
   has_secure_password
-  
-  validates :firstname, presence: true, length: { in: 2..50 }, format: { with: ALLOWED }
-  validates :lastname, presence: true, length: { in: 2..50 }, format: { with: ALLOWED }
+
+  validates :firstname,
+            presence: true,
+            length: { in: 2..50 },
+            format: { with: ALLOWED }
+
+  validates :lastname,
+            presence: true,
+            length: { in: 2..50 },
+            format: { with: ALLOWED }
+
   validates :email,
             presence: true,
             length: { maximum: 255 },
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL }
 
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :password,
+            presence: true,
+            length: { minimum: 8 }
 
   def self.authenticate_user(login_params)
-    find_by(email: login_params[:email]).try(:authenticate, login_params[:password])
+    find_by(email: login_params[:email])
+            .try(:authenticate, login_params[:password])
   end
 end
