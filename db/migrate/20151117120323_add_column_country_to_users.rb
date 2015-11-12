247f86c63067ec5a926b48b8773935847c33b9e0
ANDELA UBUNTU <ojewaleolaide@gmail.com>
2015-11-12 19:15:53 +0100

    Implement search for taskers by task. Why:
    Users could not search for taskees on the app.
    
    This change addresses the need by:
    
    Creating a search field on the home page.
    
    Creating a search for taskees of the particular task.
    
    Finding taskees by proximity to the user if user is signed in.
    
    Also fetching taskees that are not close to the users location and adding them after all close taskees have been displayed.
    
    Allowing anonymous users to search for taskees by task.
    
    Returning taskee information (including picture) when any is found.
    
    Add feature test to search.

---
+++ db/migrate/20151117120323_add_column_country_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnCountryToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :country, :string
+    add_column :users, :state, :string
+    add_column :users, :city, :string
+    add_column :users, :street_address, :string
+  end
+end
