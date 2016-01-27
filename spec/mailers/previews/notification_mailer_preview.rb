2ce9f23c87442209ec095e091e37ddd3cbe862b7
Ruth Chukwumam <aruth71@gmail.com>
2016-01-27 11:42:47 +0100

    Writing view file for email notification

--- spec/mailers/previews/notification_mailer_preview.rb
+++ spec/mailers/previews/notification_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/notification_mailer
 class NotificationMailerPreview < ActionMailer::Preview
+	
+  def send_notifications
+    NotificationMailer.send_notifications(user, task, task_category, notification_taskee, notification_tasker)
+  end
 
 end
