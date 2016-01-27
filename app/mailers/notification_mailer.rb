ff9976c92a3c4295f83b4347e9d2b313869eefce
Ruth Chukwumam <aruth71@gmail.com>
2016-01-27 10:56:07 +0100

    Started working on notification mailer

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,10 @@
+class NotificationMailer < ApplicationMailer
+  def send_notifications(user, task, task_category, notification_taskee, notification_tasker)
+    @user = user
+    @task = task
+    @task_category = task_category
+    @taskee = notification_taskee
+    @tasker = notification_tasker
+    mail to: @taskee.email, subject: "You have notifications on Workdey"
+  end
+end
