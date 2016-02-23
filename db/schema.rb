f896e0f3dfd2ccdb989b070889ea97edfc1a05f9
Amodu Temitope <amodut@gmail.com>
2016-02-23 22:05:51 +0100

    added sidebar for dashbaard

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160115184424) do
+ActiveRecord::Schema.define(version: 20160222120205) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer  "user_id"
@@ -59,18 +59,21 @@ ActiveRecord::Schema.define(version: 20160115184424) do
     t.date     "birthday"
     t.string   "phone"
     t.string   "password_digest"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.string   "user_type"
     t.string   "provider"
     t.string   "oauth_id"
     t.string   "confirm_token"
-    t.boolean  "confirmed",       default: false
+    t.boolean  "confirmed",            default: false
     t.string   "state"
     t.string   "city"
     t.string   "street_address"
     t.string   "image_url"
-    t.boolean  "has_taken_quiz",  default: false
+    t.boolean  "has_taken_quiz",       default: false
+    t.float    "longitude"
+    t.float    "latitude"
+    t.boolean  "enable_notifications", default: true
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
