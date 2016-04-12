c71c045476ca62a2df845b829e42e43fe019c071
Austin Kabiru <austin.kabiru@andela.com>
2016-04-12 14:31:20 +0300

    fix(upgrade account): resolve merge conflict
     - merge changes from upstream
     - add seed data for user plan

--- app/helpers/user_plans_helper.rb
+++ app/helpers/user_plans_helper.rb
@@ -1,2 +1,13 @@
 module UserPlansHelper
+  def novice?
+    self.user_plan && self.user_plan.name == 'novice'
+  end
+
+  def medial?
+    self.user_plan && self.user_plan.name == 'medial'
+  end
+
+  def maestro?
+    self.user_plan && self.user_plan.name == 'maestro'
+  end
 end
