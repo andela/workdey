65713c92c5f3a8deda9da0b116ef43013b9a4483
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-04-12 13:43:52 +0100

    chore(user migrations): add missing migration files

--- app/helpers/user_plans_helper.rb
+++ app/helpers/user_plans_helper.rb
@@ -1,13 +1,2 @@
 module UserPlansHelper
-  def novice?
-    user_plan && user_plan.name == "novice"
-  end
-
-  def medial?
-    user_plan && user_plan.name == "medial"
-  end
-
-  def maestro?
-    user_plan && user_plan.name == "maestro"
-  end
 end
