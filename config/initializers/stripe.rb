20983a2fd9539d0676bb79f37c75ff104ad916f2
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-04-12 16:36:38 +0100

    chore(upgrade plan): add stripe environment variables

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-    :publishable_key => Rails.application.secrets.stripe_publishable_key,
-    :secret_key      => Rails.application.secrets.stripe_secret_key
+    publishable_key:  ENV["stripe_publishable_key"],
+    secret_key: ENV["stripe_secret_key"]
 }
 
-Stripe.api_key = Rails.application.secrets.stripe_secret_key
+Stripe.api_key = ENV["stripe_secret_key"]
