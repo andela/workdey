b634c17fb061651e9347f5c0cca9d4235a03eedf
Austin Kabiru <austin.kabiru@andela.com>
2016-04-18 18:00:27 +0300

    fix(user plnan): use  in

--- app/models/user_plan.rb
+++ app/models/user_plan.rb
@@ -2,7 +2,7 @@ class UserPlan < ActiveRecord::Base
   belongs_to :user, foreign_key: :user_id
 
   def self.subscribe_user(user_id, plan)
-    find_by(user_id: user_id).update(active_until: (Date.today + 1.year),
+    find_by(user_id: user_id).update_attributes(active_until: (Date.today + 1.year),
                                      name: plan)
   end
 
