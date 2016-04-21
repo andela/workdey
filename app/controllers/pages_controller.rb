1b2ba0ab1f2819b7a8f8d7efd9f63ef82441632c
Mayowa Pitan <mayowa.pitan@andela.com>
2016-04-21 13:29:30 +0000

    feat (views): enable functionality to go the homepage as a logged-in user and back to the dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  before_action :guest_only, only: :index
+  # before_action :guest_only, only: :index
   before_action :show_notification_count, only: :search
 
   def index
