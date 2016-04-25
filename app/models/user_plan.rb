a46f4e136a8b5acae0e4dfb866236c5fe2324303
Austin Kabiru <austin.kabiru@andela.com>
2016-04-25 12:55:37 +0300

    chore(user plan model): fix hound ci violations
     - fix Gemfile.lock file
     - fix style violations on user plan model
    
    [Finishes #107830854]

--- app/models/user_plan.rb
+++ app/models/user_plan.rb
@@ -1,9 +1,12 @@
 class UserPlan < ActiveRecord::Base
-  belongs_to :user, foreign_key: :user_id
+  belongs_to :plan
+  belongs_to :user
 
   def self.subscribe_user(user_id, plan)
-    find_by(user_id: user_id).update_attributes(active_until: (Date.today + 1.year),
-                                     name: plan)
+    find_by(user_id: user_id).update(
+      active_until: (Date.today + 1.year),
+      name: plan
+    )
   end
 
   def self.set_default_user_plan
