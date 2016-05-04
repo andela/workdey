f79e9f6bababce16cbf7a4cf48a0cb5a04432a29
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-05-05 00:38:13 +0100

    test(profile completion meter): add controller specs for feature

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,18 +1,62 @@
 require "rails_helper"
 
 RSpec.describe DashboardController, type: :controller do
-  before(:all) do
-    @user = create(:user, user_type: "taskee", has_taken_quiz: true)
-  end
-
-  before(:each) do
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-  end
 
   describe "GET #home" do
-    it "returns the percentage for a taskee with skillset" do
+    context "when a taskee  without skillsets visits the dashboard " do
+    before(:each) do
+      @user = create(:user, user_type: "taskee", has_taken_quiz: true)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
       get :home
-      expect(assigns[:completion_percentage]).to eq 27
+    end
+    it "returns the percentage for a taskee with skillset" do
+      expect(assigns[:completion_percentage]).to eq 73
+    end
+    it "returns a status code of 200" do
+      expect(response.status).to eq 200
+    end
+    it "renders the home template" do
+      expect(response).to render_template :home
     end
   end
+
+  context "when a taskee with skillset visits the dashboard" do
+    before(:each) do
+      @user = create(:user, user_type: "taskee", has_taken_quiz: true)
+      @skillset = create(:skillset, user: @user )
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      get :home
+    end
+    it "returns the percentage for a taskee with skillset" do
+      expect(assigns[:completion_percentage]).to eq 82
+    end
+    it "returns a status code of 200" do
+      expect(response.status).to eq 200
+    end
+    it "renders the home template" do
+      expect(response).to render_template :home
+    end
+
+  end
+  end
+
+  context "when a tasker with complete profile visits the dashboard" do
+    before(:each) do
+      @user = create(:user, user_type: "tasker", has_taken_quiz: true, street_address: "55, moleye sreet" ,birthday: Date.today)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      get :home
+    end
+    it "returns the percentage for a taskee with skillset" do
+      expect(assigns[:completion_percentage]).to eq 100
+    end
+    it "returns a status code of 200" do
+      expect(response.status).to eq 200
+    end
+    it "renders the home template" do
+      expect(response).to render_template :home
+    end
+
+  end
+
+
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,12 @@ FactoryGirl.define do
     lastname "Pitan"
     email "mayowa.pitan@andela.com"
     password "andela rails"
-
+    gender "male"
+    phone "1234567890"
+    state "lagos"
+    city "Yaba"
+    image_url "http://res.cloudinary.com/dxoydowjy/image/upload/v1452076402"
+    
     factory "user_with_tasks" do
       transient do
         task_count 1
