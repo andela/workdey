481934a2853264b7d0395c7c243f776f7be2eece
Mayowa Pitan <mayowa.pitan@andela.com>
2016-05-28 05:53:35 +0100

    feat (model): manually change model name from taskee_bidding to bid_management

--- app/models/bid_management.rb
+++ app/models/bid_management.rb
@@ -1,4 +1,4 @@
-class TaskeeBidding < ActiveRecord::Base
+class BidManagement < ActiveRecord::Base
   belongs_to :bidding
   belongs_to :taskee, class_name: "User"
 end
--- spec/models/taskee_bidding_spec.rb
+++
@@ -1,6 +0,0 @@
-require "rails_helper"
-
-RSpec.describe TaskeeBidding, type: :model do
-  it { is_expected.to belong_to(:bidding) }
-  it { is_expected.to belong_to(:taskee).class_name("User") }
-end
