19ca431f2126fdc2159966f92d1dcb07a49357fa
Mayowa Pitan <mayowa.pitan@andela.com>
2016-06-01 18:36:12 +0100

    fix (specs): complete omniauth specs

--- spec/features/omniauth_signins_spec.rb
+++ spec/features/omniauth_signins_spec.rb
@@ -1,10 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.feature "OmniauthSignins", type: :feature do
   before(:all) do
     Capybara.default_driver = :selenium
-  end
-  before do
     @user_attributes = OmniAuth.config.mock_auth[:facebook]
     Rails.application.env_config["omniauth.auth"] = @user_attributes
   end
@@ -19,14 +17,19 @@ RSpec.feature "OmniauthSignins", type: :feature do
       expect(page).to have_button "I want to carry out tasks"
     end
   end
+
   context "when user has been registered before" do
     scenario "user should go straight to his profile page" do
       visit signin_path
+      @user = User.first_or_create_from_oauth(@user_attributes)
+      @user.user_type = "tasker"
+      @user.save
+      expect(page).to have_no_link "Edit Profile"
       click_link "Facebook"
       expect(page).to have_link @user_attributes.info.name
       click_on @user_attributes.info.name
-      expect(page).to have_content "Sign Out"
-      expect_page.to have_content "Edit Profile"
+      expect(page).to have_link "Sign Out"
+      expect(page).to have_link "Edit Profile"
     end
   end
 end
