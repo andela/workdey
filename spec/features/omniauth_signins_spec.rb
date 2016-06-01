293d206f53d4ef960aa9ada4414e8331f97c890a
Mayowa Pitan <mayowa.pitan@andela.com>
2016-06-01 17:43:54 +0100

    fix (specs): generate the spec file for omniauth sign_in

---
+++ spec/features/omniauth_signins_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "OmniauthSignins", type: :feature do
+  before(:all) do
+    Capybara.default_driver = :selenium
+  end
+  before do
+    @user_attributes = OmniAuth.config.mock_auth[:facebook]
+    Rails.application.env_config["omniauth.auth"] = @user_attributes
+  end
+
+  context "when user is not registered before" do
+    scenario "user should be able to select his role" do
+      visit signin_path
+      expect(page).to have_no_content @user_attributes.info.name
+      click_link "Facebook"
+      expect(page).to have_content @user_attributes.info.name
+      expect(page).to have_button "I need help with tasks"
+      expect(page).to have_button "I want to carry out tasks"
+    end
+  end
+  context "when user has been registered before" do
+    scenario "user should go straight to his profile page" do
+      visit signin_path
+      click_link "Facebook"
+      expect(page).to have_link @user_attributes.info.name
+      click_on @user_attributes.info.name
+      expect(page).to have_content "Sign Out"
+      expect_page.to have_content "Edit Profile"
+    end
+  end
+end
