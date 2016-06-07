6e76d63b53dcb60f38abd42bf15a5908825e1eae
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-06-07 09:40:29 +0100

    refactor(notification_of_taskees_for_task): remove the assign_task method

--- spec/features/notification_of_taskees_for_tasks_spec.rb
+++ spec/features/notification_of_taskees_for_tasks_spec.rb
@@ -11,7 +11,16 @@ RSpec.describe "Notification of taskees for new tasks", type: :feature do
   let!(:skillset) { create(:skillset, task: task, user: taskee) }
 
   before(:each) do
-    assign_task
+    log_in_with tasker.email, tasker.password
+    fill_in "searcher", with: task.name
+    click_button "Search"
+    page.all(".searched-taskee")[0].click
+    click_link "ASSIGN TASK"
+    fill_in "task_management_amount", with: "4000"
+    fill_in "task_management_task_desc", with: Faker::Lorem.sentence
+    click_button "Notify Taskee"
+    Capybara.reset_sessions!
+
     log_in_with taskee.email, taskee.password
     visit notifications_path
     page.all(".btn")[0].click
@@ -25,17 +34,5 @@ RSpec.describe "Notification of taskees for new tasks", type: :feature do
   scenario "taskee accepts a task" do
     click_on "Accept"
     expect(page).to have_content "Task accepted"
-  end
-
-  def assign_task
-    log_in_with tasker.email, tasker.password
-    fill_in "searcher", with: task.name
-    click_button "Search"
-    page.all(".searched-taskee")[0].click
-    click_link "ASSIGN TASK"
-    fill_in "task_management_amount", with: "4000"
-    fill_in "task_management_task_desc", with: Faker::Lorem.sentence
-    click_button "Notify Taskee"
-    Capybara.reset_sessions!
   end
 end
