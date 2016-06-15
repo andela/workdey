f2ac8511b5340b147a98f948befcf238c3f4f9e6
Mayowa Pitan <mayowa.pitan@andela.com>
2016-06-15 18:02:35 +0100

    feat (views): include draper gem in gemfile for the use of decorators

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,15 +1,4 @@
-class UserDecorator
-  attr_reader :user
-
-  def initialize(user)
-    @user_decorator = user
-  end
-
-  def reviewer_info(comment)
-    @reviewer_comment = comment.review
-    firstname = comment.reviewer.firstname
-    lastname = comment.reviewer.lastname
-    @reviewer_name = (firstname + " " + lastname).titleize
-    @comment_date = comment.created_at.strftime("%d %b %Y")
-  end
+class UserDecorator < Draper::Decorator
+  delegate_all
+  def
 end
