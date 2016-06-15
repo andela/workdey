2de18986df2c5847c02eb08a91ab494f3239b58a
Mayowa Pitan <mayowa.pitan@andela.com>
2016-06-15 05:50:16 +0100

    feat (models): setup model relationships

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :reviewer, class_name: "User"
+  belongs_to :reviewer, class_name: "User", foreign_key: :reviewer_id
+  belongs_to :reviewee, class_name: "User", foreign_key: :reviewee_id
+  belongs_to :task_management
+  has_many :review_comments
 end
