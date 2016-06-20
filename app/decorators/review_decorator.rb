ac628511de5b1c8a6a66e77d02bf452158351f23
Mayowa Pitan <mayowa.pitan@andela.com>
2016-06-21 00:21:44 +0100

    feat (views): decorate the review object in the show page

--- app/decorators/review_decorator.rb
+++ app/decorators/review_decorator.rb
@@ -1,11 +1,19 @@
 class ReviewDecorator < Draper::Decorator
   delegate_all
 
-  def type_of_review(id, name)
-    if id == current_user.id
+  def you_or_reviewer current_user
+    if reviewer.id == current_user.id
       "You"
     else
-      name
+      reviewer.firstname_and_lastname
+    end
+  end
+
+  def you_or_reviewee current_user
+    if reviewee.id == current_user.id
+      "You"
+    else
+      reviewee.firstname_and_lastname
     end
   end
 end
