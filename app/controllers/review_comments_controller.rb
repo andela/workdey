03961c1e1bd57a1f8b0cc093ef51a9c9f2b3dbcb
Mayowa Pitan <mayowa.pitan@andela.com>
2016-06-21 16:46:53 +0100

    feat: progress on the review comments functionality

--- app/controllers/review_comments_controller.rb
+++ app/controllers/review_comments_controller.rb
@@ -1,2 +1,18 @@
 class ReviewCommentsController < ApplicationController
+  before_action :login_required
+  def create
+    @review_comment = ReviewComment.new(review_comment_params)
+    @review_comment.user_id = current_user.id
+    if @review_comment.save
+      render :layout => false
+    else
+      #Do another thing
+    end
+  end
+
+  private
+
+  def review_comment_params
+    params.require(:review_comment).permit(:review_id, :body)
+  end
 end
