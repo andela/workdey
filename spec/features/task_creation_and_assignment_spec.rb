9c388b9dc67750fa45ea5b855b923b63b4b3c0df
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-06-21 22:45:51 +0100

    feat(task creation): ensure that only taskers have access to the task creation page

---
+++ spec/features/task_creation_and_assignment_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+RSpec.describe "Task Creation and Assignment", type: :feature do
+  before(:all) do
+    @tasker
+  end
+
+  describe "Creating task with valid details" do
+    scenario "when creating a Task with location" do
+
+    end
+  end
+end
