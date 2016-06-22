89505c733892299991a758eb4079ff89c2f3e1e6
Mayowa Pitan <mayowa.pitan@andela.com>
2016-06-22 01:40:55 +0100

    feat (views): complete review-comment functionality

--- app/controllers/review_comments_controller.rb
+++ app/controllers/review_comments_controller.rb
@@ -2,17 +2,18 @@ class ReviewCommentsController < ApplicationController
   before_action :login_required
   def create
     @review_comment = ReviewComment.new(review_comment_params)
+    @review_comment.review_id = params[:review_id]
     @review_comment.user_id = current_user.id
     if @review_comment.save
-      render :layout => false
+      render :create
     else
-      #Do another thing
+      render :error
     end
   end
 
   private
 
   def review_comment_params
-    params.require(:review_comment).permit(:review_id, :body)
+    params.require(:review_comment).permit(:body)
   end
 end
