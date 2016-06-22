fd5ad7da3ea158ea6de6b4ee6d1854cd9b4bd946
Mayowa Pitan <mayowa.pitan@andela.com>
2016-06-22 23:19:15 +0100

    chore (models): remove the review_comment model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,9 +2,8 @@ class Review < ActiveRecord::Base
   belongs_to :reviewer, class_name: "User", foreign_key: :reviewer_id
   belongs_to :reviewee, class_name: "User", foreign_key: :reviewee_id
   belongs_to :task_management
-  has_many :review_comments
 
-  validates :review,
+  validates :body,
             presence: true,
             uniqueness: { message: " - That same review has been given before" }
   validate :presence_of_rating
