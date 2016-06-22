3eb3f59fff3a2044e4099e0cef79a85a9b5ce334
Mayowa Pitan <mayowa.pitan@andela.com>
2016-06-23 00:15:43 +0100

    chore (views): remove review_comment decorators and helpers

--- app/decorators/review_comments_decorator.rb
+++
@@ -1,9 +0,0 @@
-class ReviewCommentsDecorator < Draper::CollectionDecorator
-  def render_comments
-    if empty?
-      "No comments to show yet, You may add a comment"
-    else
-      h.render partial: "review_comments/comment", collection: self
-    end
-  end
-end
