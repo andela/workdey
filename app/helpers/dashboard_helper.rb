8386bddb361da1164ebb6cb6a1a625def9985601
Mayowa Pitan <mayowa.pitan@andela.com>
2016-06-23 01:29:17 +0100

    chore (views): remove reviews link from the dashboard homepage

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -5,12 +5,4 @@ module DashboardHelper
     @reviewer_name = (firstname + " " + lastname).titleize
     @comment_date = comment.created_at.strftime("%d %b %Y")
   end
-
-  def review_someone
-    if current_user.tasker?
-      "Review a taskee"
-    else
-      "Review a tasker"
-    end
-  end
 end
