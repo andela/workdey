bf60e402ab0ecb54ba553c97f50e9ccf9af857c3
Mayowa Pitan <mayowa.pitan@andela.com>
2016-06-27 23:25:16 +0100

    chore (views): remove unnecessary files

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,10 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$('#reviewers').on('change', function() {
-  if (this.value) {
-    $.get( "/review/tasks/" + this.value, function( data ) {
-      $("#tasks").html(data);
-    });
-  }
- // or $(this).val()
-  });
