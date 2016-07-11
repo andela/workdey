8bdee024a5a49e580dab2a3f872afbc8736172e2
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-07-11 17:11:58 +0100

    feat(task creation): add create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,29 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to dashboard_path, notice: "You need has been created"
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(
+      :name,
+      :price,
+      :start_date,
+      :end_date,
+      :time,
+      :location,
+      :description,
+      :skillsets,
+      :taskee_id
+    ).merge(tasker_id: current_user.id)
+  end
 end
