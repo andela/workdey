99f5dca760ebcfee6c1529ef2efb70896b7e96e9
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-07-21 00:58:07 +0100

    feat(need search): finish search feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   has_many :skillsets
   has_many :reviews
   has_many :reviewers, class_name: "Review", foreign_key: :reviewer_id
-  has_many :tasks, through: :skillsets
+  # has_many :tasks, through: :skillsets
+  has_many :tasks, class_name: "Task", foreign_key: :tasker_id
   has_many :tasks_given, class_name: "TaskManagement", foreign_key: :taskee_id
   has_many :tasks_created, class_name: "TaskManagement", foreign_key: :tasker_id
   has_one :user_plan
