d4ffe6c60c339cc2cf9423aa0fb6332431058dd3
Mayowa Pitan <mayowa.pitan@andela.com>
2016-07-21 14:18:00 +0100

    fix (specs): pass failing tests after merge

--- spec/features/payment_gateways_spec.rb
+++ spec/features/payment_gateways_spec.rb
@@ -1,23 +1,15 @@
 require "rails_helper"
 
 RSpec.feature "PaymentGateways", type: :feature do
-  let!(:tasker) { create(:user, confirmed: true) }
-
-  before do
-    Capybara.default_driver = :selenium
-    workdey_data = Seed.new
-    workdey_data.create_all
-  end
+  let(:tasker) { create(:user, confirmed: true) }
+  let(:taskee) { create(:user, user_type: "taskee") }
 
   scenario "tasker creates a task that is unpaid for" do
     log_in_with(tasker.email, tasker.password)
-    fill_in "searcher", with: "Cleaning"
-    click_button "Search"
-    click_on("view-taskee" + User.second.id.to_s)
-    click_on "ASSIGN TASK"
-    fill_in "task_management_amount", with: "5000"
-    fill_in "task_management_task_desc", with: Faker::Lorem.sentence
-    click_on "create-task"
+    create(:task_management, tasker: tasker, taskee: taskee)
+    visit my_tasks_path
+    expect(page).to have_content("Pay for this task")
+    click_on "Pay for this task"
     expect(page).to have_content("You will have to pay before your taskee will"\
       " be notified")
     expect(page).to have_button("Pay with Card")
