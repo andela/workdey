8a95fa022228cc4e26be5c55db62551950856aa5
Mayowa Pitan <mayowa.pitan@andela.com>
2016-07-22 17:46:58 +0100

    fix: re-assign proper skillset name to session search

--- spec/features/notification_of_taskees_for_tasks_spec.rb
+++ spec/features/notification_of_taskees_for_tasks_spec.rb
@@ -4,17 +4,17 @@ require "rails_helper"
 RSpec.describe "Notification of taskees for new tasks", type: :feature do
   let(:taskee) { create(:user, user_attr.merge(user_type: "taskee")) }
   let(:tasker) { create(:user, user_attr.merge(user_type: "tasker")) }
-  let(:task) { create(:task, task_attr) }
+  let(:skillset) { create(:skillset) }
 
   before(:each) do
     create(
       :task_management,
-      task_id: task.id,
+      task_id: skillset.id,
       taskee_id: taskee.id,
       tasker_id: tasker.id,
-      status: "inactive"
+      status: "inactive",
+      paid: true
     )
-    TaskManagement.first.update_attribute(:paid, true)
     log_in_with taskee.email, taskee.password
     visit notifications_path
     page.all(".btn")[0].click
