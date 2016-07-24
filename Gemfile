d76962a885747a6e68947720e5371e087a7b91d4
Austin Kabiru <austin.kabiru@andela.com>
2016-07-24 18:45:59 +0300

    ft(add taskee references): add form validation
    [Finishes #123043311]

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem "websocket-rails", github: "moaa/websocket-rails", branch: "sync_fixes"
 gem "stripe"
 gem 'puma'
 gem 'will_paginate-materialize'
+gem 'bower'
 
 group :development, :test do
   gem "capybara"
--- app/assets/javascripts/endorsements.js
+++ app/assets/javascripts/endorsements.js
@@ -1,2 +1,29 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(function() {
+  var $toastErrorMessage = $('<span> Please choose atleast one skill</span>');
+
+  $('#new_endorsement').validate({
+    rules: {
+      "reference[skillsets][]": {
+        required: true
+      },
+      "relationship": {
+        required: true
+      },
+      "recommendation": {
+        required: true
+      }
+    },
+    messages: {
+      "reference[skillsets][]": "Please choose atleast one skill",
+      "recommendation": "Please add your recommendation."
+    },
+    errorClass: 'invalid',
+    errorPlacement: function (error, element) {
+      if ( element.is("input[type='checkbox']") ) {
+        Materialize.toast($toastErrorMessage, 4000);
+      } else {
+        element.next("label").attr("data-error", error.contents().text());
+      }
+    },
+  });
+});
