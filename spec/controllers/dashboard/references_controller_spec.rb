7815240cd3885c5eab9e4575ee19a7a65f1da98d
Austin Kabiru <austin.kabiru@andela.com>
2016-07-25 11:13:03 +0300

    ft(taskee references): add jquery-validation
    [Finishes #123043311]

--- spec/controllers/dashboard/references_controller_spec.rb
+++ spec/controllers/dashboard/references_controller_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 RSpec.describe Dashboard::ReferencesController, type: :controller do
   let(:user) { create(:user) }
-  let(:reference) { create(:reference, taskee_id: user.id) }
 
   before do
     allow_any_instance_of(ApplicationController).
@@ -38,6 +37,27 @@ RSpec.describe Dashboard::ReferencesController, type: :controller do
 
     it "assigns user's references to references" do
       expect(assigns(:references)).to eql(user.references)
+    end
+  end
+
+  describe "POST #create" do
+    let!(:req) do
+      post :create,
+           reference: {
+             firstname: Faker::Name.first_name,
+             lastname: Faker::Name.last_name,
+             email: Faker::Internet.email,
+             relationship: "Professional",
+             skillsets: ["Plumbing"]
+           }
+    end
+
+    it "returns a http created" do
+      expect(response).to have_http_status(302)
+    end
+
+    it "creates a new reference" do
+      expect(Reference.count).to eq(1)
     end
   end
 end
