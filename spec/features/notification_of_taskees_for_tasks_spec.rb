6dfcc3662ab782e3bae022df49d76cedfe84901c
Mayowa Pitan <mayowa.pitan@andela.com>
2016-07-27 04:57:11 +0100

    chore (specs): remove frozen literal string from the top of spec files

--- spec/features/notification_of_taskees_for_tasks_spec.rb
+++ spec/features/notification_of_taskees_for_tasks_spec.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 require "rails_helper"
 
 RSpec.describe "Notification of taskees for new tasks", type: :feature do
--- spec/features/profile_completion_spec.rb
+++ spec/features/profile_completion_spec.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 require "rails_helper"
 
 RSpec.describe "Profile completion meter" do
