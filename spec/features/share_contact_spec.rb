92240afa160d25f3d6b5bdd4481a4de5b86f1748
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-08-09 10:58:21 +0100

    test(contact share): add feature test for tasker viewing taskee info

--- spec/features/share_contact_spec.rb
+++ spec/features/share_contact_spec.rb
@@ -11,23 +11,46 @@ RSpec.describe "Share contact" do
       paid_for: true
     )
   end
+
   before(:each) do
     log_in_with(taskee.email, taskee.password)
   end
 
-  scenario "When taskee shares contact with tasker" do
+  describe "taskee" do
+    scenario "When taskee shares contact with tasker" do
+      share_contact
+
+      expect(page).to have_content("You have shared "\
+      "your contact with the tasker")
+      expect(task.reload.shared).to be eq true
+    end
+
+    scenario "When taskee doesn't share contact with tasker" do
+      visit my_tasks_path
+      click_on "No, don't share"
+      click_on "OK"
+
+      expect(page).to have_content("You can share your contact later")
+      expect(task.reload.shared).to be eq false
+    end
+  end
+
+  describe "tasker" do
+    scenario "when tasker views the taskee details " do
+      share_contact
+      Capybara.reset_sessions!
+      log_in_with(tasker.email, tasker.password)
+      visit notifications_path
+      click_on "view information"
+
+      expect(page).to have_content(taskee.firstname)
+      expect(page).to have_content(taskee.email)
+    end
+  end
+
+  def share_contact
     visit my_tasks_path
     click_on "Yes, share it"
     click_on "OK"
-
-    expect(task.reload.shared).to be eq true
-  end
-
-  scenario "When taskee doesn't share contact with tasker" do
-    visit my_tasks_path
-    click_on "No, don't share"
-    click_on "OK"
-
-    expect(task.reload.shared).to be eq false
   end
 end
