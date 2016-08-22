60464e0f02c28e4dd8c1ae8058c477812b587203
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-08-22 12:33:34 +0100

    fix: fix all failing tests

--- app/assets/javascripts/show_information.js
+++ app/assets/javascripts/show_information.js
@@ -1,10 +1,8 @@
- var modal_object = function(object, id){
-  $("span#name").html(object.firstname+ " "+ object.lastname);
-  $("span#phone").html(object.phone);
-  $("span#email").html(object.email);
-  
-  $.get( "/dashboard/notifications/"+ id, function( data ) {
-    console.log(data);
-  });
-  $("#modal1").openModal();
-}
+$('.modal-trigger').leanModal();
+  function modal_object(object, id){
+   $("span#name").html(object.firstname+ " "+ object.lastname);
+   $("span#phone").html(object.phone);
+   $("span#email").html(object.email);
+
+   $.get( "/dashboard/notifications/"+ id);
+ }
