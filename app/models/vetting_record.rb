471288c161182b925c8e7727d1f7fb49d2c5e3d3
morris <morris.kimani@andela.com>
2016-10-07 22:03:21 +0300

    feat(Vetting): add a VettingRecord model

--- app/models/vetting_record.rb
+++ app/models/vetting_record.rb
@@ -1,3 +1,7 @@
 class VettingRecord < ActiveRecord::Base
   belongs_to :user
+
+  def find_by_user_id(user_id)
+    self.find_by(user_id: user_id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161003195842) do
+ActiveRecord::Schema.define(version: 20161007054722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -176,6 +176,16 @@ ActiveRecord::Schema.define(version: 20161003195842) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+
+  create_table "vetting_records", force: :cascade do |t|
+    t.integer  "confidence"
+    t.integer  "skill_proficiency"
+    t.integer  "experience"
+    t.string   "interviewer_comment"
+    t.integer  "user_id"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
 
   add_foreign_key "bid_managements", "biddings"
   add_foreign_key "biddings", "tasks"
