0e9d78f658e2c44dc62bc617ac59150177235800
morris <morris.kimani@andela.com>
2016-10-07 22:11:10 +0300

    feat(Vetting): retrieve vetting_record data for the user to be rated

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,6 +2,7 @@ class RatingsController < ApplicationController
   def new
     @rating = Rating.new
     @user_id = params['user_id']
+    @vetting_record = VettingRecord.find_by_user_id(@user_id)
   end
 
   def create
