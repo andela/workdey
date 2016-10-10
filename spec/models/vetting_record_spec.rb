a892a2b2ef2fd7f49079d2672d40f832faa07e3f
morris <morris.kimani@andela.com>
2016-10-11 00:03:07 +0300

    test(Rating): add tests for the vetting model

--- spec/models/vetting_record_spec.rb
+++ spec/models/vetting_record_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe VettingRecord, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:confidence) }
+  it { should validate_presence_of(:skill_proficiency) }
+  it { should validate_presence_of(:experience) }
 end
