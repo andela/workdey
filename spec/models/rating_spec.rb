3c44c68a9647d364d731dc2c8be27d318182b559
morris <morris.kimani@andela.com>
2016-10-11 00:02:07 +0300

    test(Rating): add tests for Rating model

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Rating, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:comment) }
+  it { should validate_presence_of(:rating) }
+  it { should belong_to(:user) }
 end
