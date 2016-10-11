385655e5e7b5b4bf95ec98401d9b6133c6e6937e
Bernard Kanyolo <bernard.kanyolo@andela.com>
2016-10-11 10:36:17 +0300

    bg(models): add validations for question and response models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ActiveRecord::Base
+  validates :question, presence: true
+  validates :required, inclusion: { in: [true, false] }
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,3 +1,4 @@
 class Response < ActiveRecord::Base
   belongs_to :user
+  validates :response, presence: true
 end
