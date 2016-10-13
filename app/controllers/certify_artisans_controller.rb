8b7526ec421fd0a565db60bfe0fa339ce5e1583c
morris <morris.kimani@andela.com>
2016-10-13 07:34:08 +0300

    chore(refactor): delete duplicate file

--- app/controllers/certify_artisans_controller.rb
+++
@@ -1,6 +0,0 @@
-class CertifyArtisansController < ApplicationController
-  def get_uncertified_artisans
-    @uncertified_artisans = User.all.where(user_type: 'taskee', certified: false)
-    render 'certify_artisans'
-  end
-end
