026d0ab898bceb21ce97a8f13b846b90161e2204
morris <morris.kimani@andela.com>
2016-10-13 06:38:23 +0300

    chore(refactor): use a scope insted of class method to query db

--- app/models/vetting_record.rb
+++ app/models/vetting_record.rb
@@ -1,7 +1,4 @@
 class VettingRecord < ActiveRecord::Base
   belongs_to :user
-
-  def find_by_user_id(user_id)
-    self.find_by(user_id: user_id)
-  end
+  scope :find_by_user_id, -> (user_id) { find_by(user_id: user_id) }
 end
