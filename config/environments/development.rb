e70c18b5105c2fd575986586f968aca47731e883
Bernard Kanyolo <bernard.kanyolo@andela.com>
2016-10-14 17:36:23 +0300

    bg(mailer): fix sending and formatting of confirmation email via letter opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.delivery_method = :letter_opener
   # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.default_url_options = { host: "localhost", port: "3000" }
+  config.action_mailer.default_url_options = { host: "localhost", port: "3000" }
 
   # config.action_mailer.delivery_method =  :smtp
   # config.action_mailer.smtp_settings =
