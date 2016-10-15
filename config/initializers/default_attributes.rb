0274ecf3a9721b18e391d0a2fe69a06ddc20c239
morris <morris.kimani@andela.com>
2016-10-15 12:07:09 +0300

    bg(Profile Completion): update DEFAULT_ATTRIBUTES used when calculating profile_completion

--- config/initializers/default_attributes.rb
+++ config/initializers/default_attributes.rb
@@ -1,1 +1,1 @@
-DEFAULT_ATTR =  %w(password_digest created_at updated_at user_type provider oauth_id confirm_token confirmed has_taken_quiz enable_notifications longitude latitude id)
+DEFAULT_ATTR =  %w(password_digest created_at updated_at user_type provider oauth_id confirm_token confirmed has_taken_quiz enable_notifications longitude latitude id status rating_id)
