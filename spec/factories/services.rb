f55506bece2bd4ed75a6e227fcf799ec20280a90
andela-vkironde <victor.kironde@andela.com>
2016-10-26 15:00:18 +0300

    feat(quote): Add Service Model

---
+++ spec/factories/services.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :service do
+    tasker_id 1
+    artisan_id 1
+    skillset_id 1
+    title "MyString"
+    description "MyString"
+    start_date "2016-10-25 20:53:19"
+    end_date "2016-10-25 20:53:19"
+    duration "2016-10-25 20:53:19"
+    status 1
+  end
+end
