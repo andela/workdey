a6819ed3bc1ed148a429d6e5af501c803b470c9a
Bernard Kanyolo <bernard.kanyolo@andela.com>
2016-10-26 14:50:16 +0300

    chore: generate active job for service assignment

---
+++ app/jobs/service_assignment_job.rb
@@ -0,0 +1,23 @@
+class ServiceAssignmentJob < ActiveJob::Base
+  queue_as :default
+  
+  def perform(service)
+    if service.accepted?
+      # break task
+    elsif service.day_old?
+      #stop execution
+      #send inform mail to tasker
+    elsif gone_through_all_artisans?
+      #stop execution
+      #send inform mail to tasker
+    else
+      # get highest rated available
+      # assign
+      # send notification to newly assigned artisan
+      # send notification to old artisan, if any
+      # call yourself here recursively in 30 minutes to check if artisan accepted
+    end
+  end
+
+  ServiceAssignment [service_id, artisan_id]
+end
