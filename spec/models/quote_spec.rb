c2d363574c6585af0cc56e295be3a3e4b3382163
morris <morris.kimani@andela.com>
2016-11-02 13:49:28 +0300

    test(Quotes): add tests for the quote model

--- spec/models/quote_spec.rb
+++ spec/models/quote_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Quote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of :artisan_id }
+  it { is_expected.to validate_presence_of :service_id }
+  it { is_expected.to validate_presence_of :quoted_value }
+  it { is_expected.to belong_to :artisan }
+  it { is_expected.to belong_to :service }
+  it { is_expected.to define_enum_for :status }
 end
