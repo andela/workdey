680e8dd8ffe53202d19207e8c32fba246b542b46
morris <morris.kimani@andela.com>
2016-11-07 21:29:25 +0300

    fix(Quote): fix issue where a service request notification was being marked as read even before the artisan accepts or rejects the request

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -28,9 +28,9 @@ class NotificationsController < ApplicationController
     end
     record = @notification.notifiable
     if record.update_attributes(@notifiable_attr_to_update.symbolize_keys)
+      @notification.update_as_read force_update: true
       if @reply_to_sender == true
         @notification.reply_to_sender(@message, "new_task")
-        @notification.update_as_read
       end
       render json: { message: "success" }
     end
