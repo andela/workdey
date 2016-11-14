3cfddd135e48ff6b68d119c28ffbc250f2dc9d69
Hannah <hannah.masila@andela.com>
2016-11-14 11:32:43 +0300

    ch(application): replace weak_yes and weak_no with yes and no respectively

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,7 +56,7 @@ class User < ActiveRecord::Base
   scope :artisans, -> { where(user_type: "artisan") }
 
   enum status: [:not_reviewed, :strong_yes,
-                :weak_yes, :weak_no, :strong_no, :certified]
+                :yes, :no, :strong_no, :certified]
 
   def self.first_or_create_from_oauth(auth)
     where(email: auth.info.email).first_or_create do |u|
@@ -139,7 +139,7 @@ class User < ActiveRecord::Base
   end
 
   def accepted?
-    strong_yes? || weak_yes?
+    strong_yes? || yes?
   end
 
   def pending_artisan?
