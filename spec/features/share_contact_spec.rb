4ba6d12a82cec2961163b281bd05b65544daaa17
andela-vkironde <victor.kironde@andela.com>
2016-11-16 19:40:16 +0300

    bg(): remove syntax error from share contact spec

--- spec/features/share_contact_spec.rb
+++ spec/features/share_contact_spec.rb
@@ -37,20 +37,6 @@ RSpec.describe "Share contact", js: true do
 
   end
 
-  # describe "tasker" do
-  #   scenario "when tasker views the artisan details " do
-  #     share_contact
-  #     Capybara.reset_sessions!
-  #     log_in_with(tasker.email, tasker.password)
-  #     visit notifications_path
-  #     click_on "view quote"
-  #     click_button "Accept"
-  #
-  #     expect(page).to have_content(artisan.firstname)
-  #     expect(page).to have_content(artisan.email)
-  #   end
-  # end
-
      describe "tasker" do
        scenario "when tasker views the artisan's quote " do
            Capybara.reset_sessions!
@@ -71,9 +57,5 @@ RSpec.describe "Share contact", js: true do
     sleep 1.5
     share ? click_on("Yes, share it") : click_on("No, don't share")
   end
-
-  def share_quote(share = true)
-    visit my_tasks_path
-    click_on ""
 
 end
