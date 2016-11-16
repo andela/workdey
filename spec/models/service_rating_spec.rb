1da6c6077237e5f2df7d15cdab8887f8a037bc76
andela-amagana <alex.magana@andela.com>
2016-11-16 18:09:24 +0300

    feat(service_ratings): add model specs for ServiceRating model

---
+++ spec/models/service_rating_spec.rb
@@ -0,0 +1,121 @@
+require "rails_helper"
+
+RSpec.describe ServiceRating, type: :model do
+  before(:each) do
+    @tasker = create(:user)
+    @artisan = create(:user, user_type: "artisan")
+    @service = create(:service, tasker: @tasker, artisan: @artisan)
+    @service_second = create(:service, tasker: @tasker, artisan: @artisan)
+  end
+
+  describe "associations" do
+    it { is_expected.to belong_to(:service) }
+  end
+
+  describe "validates rating" do
+    it { is_expected.to validate_presence_of :rating }
+    it do
+      is_expected.to validate_inclusion_of(:rating).
+        in_array([1, 2, 3, 4, 5])
+    end
+  end
+
+  describe "validates private_feedback" do
+    it { is_expected.to validate_presence_of :private_feedback }
+    it do
+      is_expected.to validate_length_of(:private_feedback).is_at_least(3).
+        with_message("Too short. The minimum length is 3 characters.")
+    end
+  end
+
+  describe "validates public_feedback" do
+    it { is_expected.to validate_presence_of :public_feedback }
+    it do
+      is_expected.to validate_length_of(:public_feedback).is_at_least(3).
+        with_message("Too short. The minimum length is 3 characters.")
+    end
+  end
+
+  describe "validates rating type" do
+    it { is_expected.to validate_presence_of :category }
+  end
+
+  describe "gets ratings by user" do
+    it "returns ratings of an artisan" do
+      create :service_rating, service: @service
+
+      artisan_ratings = ServiceRating.get_ratings(@artisan)
+
+      expect(artisan_ratings[0].service.artisan.id).to eq @artisan.id
+    end
+  end
+
+  describe "computes average rating" do
+    it "returns the average rating of an artisan" do
+      create :service_rating, rating: 3, service: @service
+      create :service_rating, rating: 4, service: @service_second
+
+      artisan_average_rating = ServiceRating.compute_average_rating(@artisan)
+
+      expect(artisan_average_rating).to eq 3.5
+    end
+  end
+
+  describe "gets an artisan's rating" do
+    it "returns the rating of an artisan" do
+      create :service_rating, rating: 3, service: @service
+
+      artisan_rating = ServiceRating.get_artisan_rating(@artisan)
+
+      expect(artisan_rating[0].rating).to eq 3
+    end
+  end
+
+  describe "gets a tasker's rating" do
+    it "returns the rating of a tasker" do
+      create(
+        :service_rating,
+        rating: 5,
+        category: ServiceRating.categories[:artisan_to_tasker],
+        service: @service
+      )
+
+      tasker_rating = ServiceRating.get_tasker_rating(@tasker)
+
+      expect(tasker_rating[0].rating).to eq 5
+    end
+  end
+
+  describe "get an artisan's average rating" do
+    it "returns the average rating of an artisan" do
+      create :service_rating, rating: 2, service: @service
+      create :service_rating, rating: 4, service: @service_second
+
+      artisan_average = ServiceRating.get_artisan_average_rating(@artisan)
+
+      expect(artisan_average).to eq 3
+    end
+  end
+
+  describe "gets a tasker's average rating" do
+    it "returns the average rating of a tasker" do
+      create(
+        :service_rating,
+        rating: 3,
+        category: ServiceRating.categories[:artisan_to_tasker],
+        service: @service
+      )
+
+      create(
+        :service_rating,
+        rating: 2,
+        category: ServiceRating.categories[:artisan_to_tasker],
+        service: @service
+      )
+
+      tasker_rating = ServiceRating.get_tasker_average_rating(@tasker)
+
+      expect(tasker_rating).to eq 2.5
+    end
+  end
+end
