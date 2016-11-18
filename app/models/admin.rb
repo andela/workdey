07f9ca4d5df94c51119b4513d1e4c5f0c16b12d1
Bernard Kanyolo <bernard.kanyolo@andela.com>
2016-11-18 11:05:55 +0300

    bg(views): fix vadmin vieiwng applicants without questionnaires on reveiew applicants dashboard

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,5 @@
 class Admin < User
   scope :get_applicants, lambda {
-    where(user_type: "artisan", status: 0)
+    where(user_type: "artisan", status: 0, has_taken_questionnaire: true)
   }
 end
