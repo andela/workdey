af88f23c7fa5dfef4732dedba34fc9db2926b304
Bernard Kanyolo <bernard.kanyolo@andela.com>
2016-11-21 14:25:47 +0300

    ft(views): enable promoting and demoting of questions from question index page

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,5 +1,23 @@
 class Admin::QuestionsController < ApplicationController
+  before_action :find_question
+    
   def index
     @questions = Question.ranked
+  end
+
+  def promote
+    @question.promote_rank
+    redirect_to admin_questions_path
+  end
+
+  def demote
+    @question.demote_rank
+    redirect_to admin_questions_path
+  end
+
+  private
+
+  def find_question
+    @question = Question.find_by(id: params[:id])
   end
 end
