2e0aadff958c65d2c541b1e240eb10a200b7e305
morris <morris.kimani@andela.com>
2016-11-23 00:31:50 +0300

    test(Service Factory): modify the service factory to automatically create a service's tasker when creating a service

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :user do
+  factory :user, aliases: [:tasker] do
     firstname { Faker::Name.first_name }
     lastname { Faker::Name.last_name }
     email { Faker::Internet.free_email }
