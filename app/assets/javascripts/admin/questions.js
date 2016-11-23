381f48b1ce868c5dd5e802a7370397af3583c4d7
Bernard Kanyolo <bernard.kanyolo@andela.com>
2016-11-23 18:14:34 +0300

    ft(views): enable adding and removing of question options on new page

--- app/assets/javascripts/admin/questions.js
+++ app/assets/javascripts/admin/questions.js
@@ -5,3 +5,24 @@ $(document).ready(function() {
     dismissible: false
   });
 });
+
+var option_number = 0
+
+function addInputBox(text) {
+  //$('#question_options').append("<br />");
+  $('#question_options').append("<div id='" + option_number + "' class='input-field row'></div>");
+  $('#' + option_number).append("<input value='" + text + "' type='text' name='question[options][]' class='col s9 m9 l9'></input>");
+  $('#' + option_number).append(
+    "<a class='waves-effect waves-light btn red small col s3 m3 l3' onclick='removeInputBox(" + option_number + ")'><i class='material-icons left'>cancel</i>Remove</a>"
+  );
+  option_number++;
+}
+
+function removeInputBox(id) {
+  $('#' + id).remove();
+  option_number--;
+}
+
+$('#add_option').click(function (event) {
+  event.preventDefault(); // Prevent link from following its href
+});
